67b9427465e9064f94d9814d792114687ea58638
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2024-03-05 16:07:39 +0200

    Initial commit

---
+++ software_design_lab2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace software_design_lab2_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
